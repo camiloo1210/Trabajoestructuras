71ff04055d5aaa0227c388ccd4317e306f7a5887
Laboratorio <laboratorio@udla-ec.int>
2023-06-19 15:50:53 -0500

    Funcionalidad usuario

--- estructuras.c
+++ estructuras.c
@@ -13,6 +13,8 @@ struct alumno
 
 int main()
 {
+    for (int  i = 0; i < 5; i++)
+    {
     struct alumno alum1;
     printf("Ingrese su nombre\n");
     gets(alum1.nombre);
@@ -30,92 +32,7 @@ int main()
     printf("Su edad es :\n");
     puts(alum1.edad);
     printf("Su carrera es : \n");
-    puts(alum1.carrera);
-
-    printf("Segundo alumno");
-
-    struct alumno alum2;
-    printf("Ingrese su nombre\n");
-    gets(alum1.nombre);
-    printf("Ingrese su direccion\n");
-    gets(alum1.direccion);
-    printf("Ingrese su edad\n");
-    scanf("%i",&alum1.edad);
-    printf("Ingrese su carrera\n");
-    fflush(stdin);
-    gets(alum1.carrera);
-    printf("Su nombre es :\n");
-    puts(alum1.nombre);
-    printf("Su direccion es : \n");
-    puts(alum1.direccion);
-    printf("Su edad es :\n");
-    puts(alum1.edad);
-    printf("Su carrera es : \n");
-    puts(alum1.carrera);
-
-    printf("Tercer alumno");
-    
-    struct alumno alum3;
-    printf("Ingrese su nombre\n");
-    gets(alum1.nombre);
-    printf("Ingrese su direccion\n");
-    gets(alum1.direccion);
-    printf("Ingrese su edad\n");
-    scanf("%i",&alum1.edad);
-    printf("Ingrese su carrera\n");
-    fflush(stdin);
-    gets(alum1.carrera);
-    printf("Su nombre es :\n");
-    puts(alum1.nombre);
-    printf("Su direccion es : \n");
-    puts(alum1.direccion);
-    printf("Su edad es :\n");
-    puts(alum1.edad);
-    printf("Su carrera es : \n");
-    puts(alum1.carrera);
-
-    printf("Cuarto alumno");
-    
-    struct alumno alum4;
-    printf("Ingrese su nombre\n");
-    gets(alum1.nombre);
-    printf("Ingrese su direccion\n");
-    gets(alum1.direccion);
-    printf("Ingrese su edad\n");
-    scanf("%i",&alum1.edad);
-    printf("Ingrese su carrera\n");
-    fflush(stdin);
-    gets(alum1.carrera);
-    printf("Su nombre es :\n");
-    puts(alum1.nombre);
-    printf("Su direccion es : \n");
-    puts(alum1.direccion);
-    printf("Su edad es :\n");
-    puts(alum1.edad);
-    printf("Su carrera es : \n");
-    puts(alum1.carrera);
-    return 0;
-
-    printf("Quinto alumno");
-    
-    struct alumno alum5;
-    printf("Ingrese su nombre\n");
-    gets(alum1.nombre);
-    printf("Ingrese su direccion\n");
-    gets(alum1.direccion);
-    printf("Ingrese su edad\n");
-    scanf("%i",&alum1.edad);
-    printf("Ingrese su carrera\n");
-    fflush(stdin);
-    gets(alum1.carrera);
-    printf("Su nombre es :\n");
-    puts(alum1.nombre);
-    printf("Su direccion es : \n");
-    puts(alum1.direccion);
-    printf("Su edad es :\n");
-    puts(alum1.edad);
-    printf("Su carrera es : \n");
     puts(alum1.carrera);    
-    
+    }
 }
 
